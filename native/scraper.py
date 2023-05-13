4b61574fdb4971b29baee4b3f65381b52782b678
Anthony Olebe <2r4u666@gmail.com>
2023-05-13 11:59:51 -0700

    added template for options page

--- native/scraper.py
+++ native/scraper.py
@@ -22,5 +22,9 @@ print(calendarLink)
 driver.get(calendarLink)
 calendarContent=driver.page_source
 calendar=BeautifulSoup(calendarContent,"html.parser")
-with open("%s.txt" %site[len(site)-20], "w", encoding='utf-8') as text_file:
-    text_file.write(calendar.prettify())
+scripts = calendar.find_all("script",string=True)
+
+
+    
+# with open("%s.txt" %site[len(site)-20], "w", encoding='utf-8') as text_file:
+#     text_file.write(calendar.prettify())
