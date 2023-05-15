023c08977300f3e0de847f7f2920ed50921fc5f9
Anthony Olebe <2r4u666@gmail.com>
2023-05-15 15:23:20 -0700

    progress i guess

--- native/scraper.bat
+++ native/scraper.bat
@@ -1,5 +1,4 @@
 @echo off
-:REG ADD "HKEY_CURRENT_USER\SOFTWARE\Google\Chrome\NativeMessagingHosts\com.edb.scraper" /ve /t REG_SZ /d "C:\Users\2r4u6\Documents\apcs\edb\native\scraper.json" /f
-pip install selenium
-pip install beautifulsoup4
+::pip install selenium
+::pip install beautifulsoup4
 start scraper.py
--- native/scraper.py
+++ native/scraper.py
@@ -7,24 +7,26 @@ service = Service(executable_path="chromedriver.exe")
 
 driver = webdriver.Chrome(service=service)
 
-site="https://sites.google.com/pleasantonusd.net/biology-fewster/home"
+# site="https://sites.google.com/pleasantonusd.net/biology-fewster/home"
 
-driver.get(site)
+# driver.get(site)
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
-with open("%s.txt" %site[len(site)-20], "w", encoding='utf-8') as text_file:
-    text_file.write(soup.prettify())
+# content = driver.page_source
+# soup = BeautifulSoup(content, "html.parser")
+# with open("%s.txt" %site[len(site)-20], "w", encoding='utf-8') as text_file:
+#     text_file.write(soup.prettify())
 
-calendarLink =[item["data-embed-open-url"] for item in soup.find_all() if "data-embed-open-url" in item.attrs][0]
-print(calendarLink)
-
+# calendarLink =[item["data-embed-open-url"] for item in soup.find_all() if "data-embed-open-url" in item.attrs][0]
+# print(calendarLink)
+calendarLink="https://drive.google.com/open?authuser=0&id=1AwFI1BD0pUJygVVUk2-rtC93HKBxQWuKA6tdknMq2p8"
 driver.get(calendarLink)
 calendarContent=driver.page_source
 calendar=BeautifulSoup(calendarContent,"html.parser")
 
 scripts = calendar.find_all("script",string=True)
 
-with open("%s.txt" %site[len(site)-20], "a", encoding='utf-8') as text_file:
+with open("%s.txt" %calendarLink[len(calendarLink)-20], "a", encoding='utf-8') as text_file:
     for s in scripts:
         text_file.write(calendar.prettify())
+
+#contents of doc can be found in <script nonce="" type="text/javascript">
