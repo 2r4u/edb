575611fd53be0c34cec3dac86817fc6491978ebf
Anthony Olebe <2r4u666@gmail.com>
2023-05-19 15:25:39 -0700

    added txt parsing to json

--- native/txt-parser.py
+++ native/txt-parser.py
@@ -1,13 +1,27 @@
 import re
-
+import json
+from unidecode import unidecode
+dp1="\d/\d"
+dp2="\d/\d\d"
+calendarDict={}
 with open("calendars\PC2223 Student Calendar.txt") as calendar:
+    record=False
+    key=""
+    content=""
     for line in calendar:
         if not len(line) - len(line.lstrip())>0:
-            dates.append(line)
+            record=False
+            calendarDict[key]=content
+            key=""
+            content=""
+            if re.match(dp1,line[0:4]):
+                key=line.strip()
+                record=True
+            elif re.match(dp2,line[0:5]):
+                key=line.strip()
+                record=True
+        elif record:
+            content+=unidecode(line.strip())+" "
 
-dp ="\d\\\d"
-#TODO: use regular expressions to filter dates
-print(dates)
-for d in dates:
-    if re.match(dp,d[0:4]):
-        print(d)
+with open("calendar.json", "w") as jsonfile:
+    json.dump( calendarDict, jsonfile)
