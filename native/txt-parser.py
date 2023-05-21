327fae6739eff9ca6247e010d1650be4ff11d7c7
Anthony Olebe <2r4u666@gmail.com>
2023-05-20 20:47:34 -0700

    major improvements to txt parser :)

--- native/txt-parser.py
+++ native/txt-parser.py
@@ -1,6 +1,5 @@
 import re
 import json
-from unidecode import unidecode
 dp1="\d/\d"
 dp2="\d/\d\d"
 calendarDict={}
@@ -9,19 +8,21 @@ with open("calendars\PC2223 Student Calendar.txt") as calendar:
     key=""
     content=""
     for line in calendar:
-        if not len(line) - len(line.lstrip())>0:
-            record=False
-            calendarDict[key]=content
-            key=""
-            content=""
-            if re.match(dp1,line[0:4]):
-                key=line.strip()
+        ln =re.sub("[^\x00-\x7F]+","",line)
+        if not len(ln) - len(ln.lstrip())>0:
+            if record:
+                record=False
+                calendarDict[key]=content.strip()
+                key=""
+                content=""
+            if re.match(dp1,ln[0:4]):
+                key=ln.strip()
                 record=True
-            elif re.match(dp2,line[0:5]):
-                key=line.strip()
+            elif re.match(dp2,ln[0:5]):
+                key=ln.strip()
                 record=True
         elif record:
-            content+=unidecode(line.strip())+" "
+            content+=ln.strip()+" "
 
 with open("calendar.json", "w") as jsonfile:
     json.dump( calendarDict, jsonfile)
