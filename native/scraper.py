99921677b5140a1806de9c3f1a5531d67e5de92d
Anthony Olebe <2r4u666@gmail.com>
2023-05-22 12:23:08 -0700

    finished calendar parsing process :))))

--- native/scraper.py
+++ native/scraper.py
@@ -7,26 +7,14 @@ import time
 service = Service(executable_path="chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", {
-  "download.default_directory": "C:\\Users\\2r4u6\\Documents\\apcs\\edb\\native\\calendars",
+  "download.default_directory": "C:\\Users\\2r4u6\\Documents\\apcs\\edb\\native\\txt-calendars",
   "download.prompt_for_download":False
 })
 
 driver = webdriver.Chrome(service=service, options=options)
 
-
-# site="https://sites.google.com/pleasantonusd.net/biology-fewster/home"
-
-# driver.get(site)
-
-# content = driver.page_source
-# soup = BeautifulSoup(content, "html.parser")
-# with open("%s.txt" %site[len(site)-20], "w", encoding='utf-8') as text_file:
-#     text_file.write(soup.prettify())
-
-# calendarLink =[item["data-embed-open-url"] for item in soup.find_all() if "data-embed-open-url" in item.attrs][0]
-# print(calendarLink)
-calendarLink="https://docs.google.com/document/d/1AwFI1BD0pUJygVVUk2-rtC93HKBxQWuKA6tdknMq2p8"
-#driver.get(calendarLink)
-time.sleep(5)
+calendarLink="https://docs.google.com/document/export?format=txt&id=1JivTyrwnIbi8V23sukPgTewOuND-HTYKLVHc8S3BVSc"
+driver.get(calendarLink)
+time.sleep(3)
 
 #use this link:https://docs.google.com/document/u/1/export?format=txt + the id of the doc you want to download
--- native/txt-parser.py
+++ native/txt-parser.py
@@ -1,28 +1,35 @@
-import re
-import json
+import re, json, os
+
+#set regex patterns and file directory
 dp1="\d/\d"
 dp2="\d/\d\d"
-calendarDict={}
-with open("calendars\PC2223 Student Calendar.txt") as calendar:
-    record=False
-    key=""
-    content=""
-    for line in calendar:
-        ln =re.sub("[^\x00-\x7F]+","",line)
-        if not len(ln) - len(ln.lstrip())>0:
-            if record:
-                record=False
-                calendarDict[key]=content.strip()
-                key=""
-                content=""
-            if re.match(dp1,ln[0:4]):
-                key=ln.strip()
-                record=True
-            elif re.match(dp2,ln[0:5]):
-                key=ln.strip()
-                record=True
-        elif record:
-            content+=ln.strip()+" "
+directory="txt-calendars/"
 
-with open("calendar.json", "w") as jsonfile:
-    json.dump( calendarDict, jsonfile)
+#loop through txt files
+for filename in os.listdir(directory):
+    calendarDict={}
+    path="txt-calendars/"
+    #loop to format and record calendar in dictionary
+    with open(path+filename) as calendar:
+        record=False
+        key=""
+        content=""
+        for line in calendar:
+            ln =re.sub("[^\x00-\x7F]+","",line)
+            if not len(ln) - len(ln.lstrip())>0:
+                if record:
+                    record=False
+                    calendarDict[key]=content.strip()
+                    key=""
+                    content=""
+                if re.match(dp1,ln[0:4]):
+                    key=ln.strip()
+                    record=True
+                elif re.match(dp2,ln[0:5]):
+                    key=ln.strip()
+                    record=True
+            elif record:
+                content+=ln.strip()+" "
+    #dumps the formatted calendar into a json file wiht the same name
+    with open("json-calendars/%s.json" % os.path.splitext(filename)[0], "w") as jsonfile:
+        json.dump( calendarDict, jsonfile)
