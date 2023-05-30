39a2b36ea81727b35b080b0a3787b4c4b46183bf
Anthony Olebe <2r4u666@gmail.com>
2023-05-30 09:23:27 -0700

    assignments show up on classroom B)

--- content-script.js
+++ content-script.js
@@ -20,6 +20,9 @@ function inject() {
                 console.log("loaded data: " + JSON.parse(item))
                 cjdict = JSON.parse(item);
                 const date=new Date();
+                let upcomingAssignments=[];
+                let digits="123456789".split();
+                digits.push("10","11","12");
                 let assingnmentAreas = Array.from(document.getElementsByClassName("lziZub"));
                 for (let i = 0; i < assingnmentAreas.length; i++) {
                     if(assingnmentAreas[i].classList.contains("sdDCme")){
@@ -28,12 +31,22 @@ function inject() {
                     }
                     let calendar= cjdict[classes[i]];
                     for(const key in calendar){
-                        let cdate=new Date(key);
-                        if(date.getMonth)
-                        //TODO: parse dates, show three closest assignments
-                        const span=document.createElement("span");
-                        span.textContent=contents[i];
-                        assingnmentAreas[i].appendChild(span);
+                        let ykey=""
+                        if(digits.indexOf(key.substring(0,1))>=7){
+                            ykey=key+"/"+(date.getFullYear()-1);
+                        }
+                        else if(digits.indexOf(key.substring(0,2))>-1){
+                            ykey=key+"/"+(date.getFullYear()-1);
+                        }
+                        else{
+                            ykey=key+"/"+date.getFullYear();
+                        }
+                        if(timeUntil(Date.parse(ykey))<=0&&timeUntil(Date.parse(ykey))>-7){
+                            const span=document.createElement("span");
+                            span.textContent=key+" - "+calendar[key];
+                            assingnmentAreas[i].appendChild(span);
+                            assingnmentAreas[i].appendChild(document.createElement("br"));
+                        }
                     }
                     
                 }
@@ -61,6 +74,17 @@ function scan() {
     }
 
 }
+function timeUntil(ms){
+    // Calculate milliseconds in a day
+    const date=new Date();
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    let msUntil=(Date.parse(date)-ms);
+    return(Math.round(msUntil/day));
+}
+
 let classes =[];
 retrieveData("classes").then(function(item){
     classes=JSON.parse(item);
